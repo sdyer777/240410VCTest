50eb10f91a27d6a267650268755d3462dfe81999
sdyer777 <sdyer777@yahoo.com>
2024-04-17 17:48:48 -0400

    Code cleaup.

--- 240410VCTest/MenuViewController.swift
+++ 240410VCTest/MenuViewController.swift
@@ -20,16 +20,12 @@ class MenuViewController: UIViewController {
         gameButton.setTitle("Play Game", for: .normal)
         gameButton.backgroundColor = UIColor.lightGray
         gameButton.titleLabel?.textColor = UIColor.black
-        // Customize your button here
-//        derButton.titleLabel?.font = UIFont.init(name: "HelveticaNeue-thin", size: 30)
         gameButton.layer.borderColor = UIColor.darkGray.cgColor
         gameButton.layer.borderWidth = 1.5
         gameButton.layer.cornerRadius = 10.0
         gameButton.layer.masksToBounds = true
         gameButton.translatesAutoresizingMaskIntoConstraints = false
         gameButton.addTarget(self, action: #selector(gameButtonTapped(_:)), for: .touchUpInside)
-//        gameButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
-//        gameButton.center = CGPoint(x: view.frame.width/2, y: 300)
         return gameButton
     }()
         
@@ -38,16 +34,12 @@ class MenuViewController: UIViewController {
         helpButton.setTitle("Help", for: .normal)
         helpButton.backgroundColor = UIColor.lightGray
         helpButton.titleLabel?.textColor = UIColor.black
-        // Customize your button here
-//        derButton.titleLabel?.font = UIFont.init(name: "HelveticaNeue-thin", size: 30)
         helpButton.layer.borderColor = UIColor.darkGray.cgColor
         helpButton.layer.borderWidth = 1.5
         helpButton.layer.cornerRadius = 10.0
         helpButton.layer.masksToBounds = true
         helpButton.translatesAutoresizingMaskIntoConstraints = false
         helpButton.addTarget(self, action: #selector(helpButtonTapped(_:)), for: .touchUpInside)
-//        helpButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
-//        helpButton.center = CGPoint(x: view.frame.width/2, y: 375)
         return helpButton
     }()
         
@@ -56,16 +48,12 @@ class MenuViewController: UIViewController {
         hideAdsButton.setTitle("Hide Ads", for: .normal)
         hideAdsButton.backgroundColor = UIColor.lightGray
         hideAdsButton.titleLabel?.textColor = UIColor.black
-        // Customize your button here
-//        derButton.titleLabel?.font = UIFont.init(name: "HelveticaNeue-thin", size: 30)
         hideAdsButton.layer.borderColor = UIColor.darkGray.cgColor
         hideAdsButton.layer.borderWidth = 1.5
         hideAdsButton.layer.cornerRadius = 10.0
         hideAdsButton.layer.masksToBounds = true
         hideAdsButton.translatesAutoresizingMaskIntoConstraints = false
         hideAdsButton.addTarget(self, action: #selector(hideAdsButtonTapped(_:)), for: .touchUpInside)
-//        hideAdsButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
-//        hideAdsButton.center = CGPoint(x: view.frame.width/2, y: 450)
         return hideAdsButton
     }()
         
@@ -74,16 +62,12 @@ class MenuViewController: UIViewController {
         rexButton.setTitle("Rex", for: .normal)
         rexButton.backgroundColor = UIColor.lightGray
         rexButton.titleLabel?.textColor = UIColor.black
-        // Customize your button here
-//        derButton.titleLabel?.font = UIFont.init(name: "HelveticaNeue-thin", size: 30)
         rexButton.layer.borderColor = UIColor.darkGray.cgColor
         rexButton.layer.borderWidth = 1.5
         rexButton.layer.cornerRadius = 10.0
         rexButton.layer.masksToBounds = true
         rexButton.translatesAutoresizingMaskIntoConstraints = false
         rexButton.addTarget(self, action: #selector(rexButtonTapped(_:)), for: .touchUpInside)
-//        rexButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
-//        rexButton.center = CGPoint(x: view.frame.width/2, y: 525)
         return rexButton
     }()
         
@@ -141,16 +125,6 @@ class MenuViewController: UIViewController {
         print("MenuViewController - helpButton Was Tapped")
         
         vcDelegate.presentHelpScreen()
-
-/*
-        let vc  = HelpPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-        vc.modalPresentationStyle = .overFullScreen
-        vc.view.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.8)
-        vc.modalTransitionStyle = .crossDissolve
-//        vc.sceneManagerDelegate = self
-        self.show(vc, sender: self)
-*/
-        
     }
     
     @objc func hideAdsButtonTapped(_ sender: UIButton) {
